1f0f80b38072c8e7d4dd158b1dfa4a26b4e4599c
sandip <[sandipsanjel03@gmail.com]>
2022-04-22 22:32:56 +0545

    commit

--- Es-classes/Assignment/Assignment3..js
+++ Es-classes/Assignment/Assignment3..js
@@ -1,9 +1,18 @@
-function calculator(bill_ammount){
-    if (bill_ammount >50 && bill_ammount <300){
-        let tip=(bill_ammount *10)/100;
-        
-
+function tipCalculator(amount){
+    // Tip 10% if the amount is between 50 and 300
+    if (50 <= amount && amount <= 300){
+        var tip = amount * 0.1;
     }
 
+    //For different value tip is 15%
+    else{
+        var tip = amount * 0.15;
+    }
 
+    console.log(`The bill was Rs. ${amount}, the tip was Rs. ${tip}, and the total is Rs. ${amount + tip}`);
 }
+
+tipCalculator(200);
+tipCalculator(275);
+tipCalculator(40);
+tipCalculator(430);
